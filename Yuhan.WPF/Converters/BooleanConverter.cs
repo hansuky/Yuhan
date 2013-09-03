d98d402655f8b08bd1697989e7f85eaa914d9a9c
Yuhan <hansuky@hotmail.com>
2013-09-03 11:35:37 +0900

    BooleanConverter was Added

---
+++ Yuhan.WPF/Converters/BooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Yuhan.WPF.Converters
+{
+    public class BooleanConverter<T> : IValueConverter
+    {
+        public T True { get; set; }
+        public T False { get; set; }
+        public BooleanConverter(T trueValue, T falseValue)
+        {
+            True = trueValue;
+            False = falseValue;
+        }
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is bool && ((bool)value) ? True : False;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is T && EqualityComparer<T>.Default.Equals((T) value, True);
+        }
+    }
+}
---
+++ Yuhan.WPF/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Yuhan.WPF.Converters
+{
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed) { }
+    }
+}
