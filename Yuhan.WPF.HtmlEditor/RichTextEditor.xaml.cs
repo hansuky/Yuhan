dd086c32284aa31c283c542a4a5553de0ecf829c
Yuhan <hansuky@hotmail.com>
2013-09-06 13:20:37 +0900

    WPF Html Editor was added

---
+++ Yuhan.WPF.HtmlEditor/RichTextEditor.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yuhan.WPF.HtmlEditor
+{
+    /// <summary>
+    /// Interaction logic for BindableRichTextbox.xaml
+    /// </summary>
+    public partial class RichTextEditor : UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+          DependencyProperty.Register("Text", typeof(string), typeof(RichTextEditor),
+          new PropertyMetadata(string.Empty));
+        
+        public RichTextEditor()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return GetValue(TextProperty) as string; }
+            set { 
+                SetValue(TextProperty, value);
+            }
+        }
+    }
+}
