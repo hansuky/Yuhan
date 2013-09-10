353cd2e95d782dfa8cae2ec46e3fd23a77856a1d
Yuhan <hansuky@hotmail.com>
2013-09-10 09:29:00 +0900

    ICollection RemoveRange Extension added

--- Yuhan.Common/Extensions/ICollectionExtension.cs
+++ Yuhan.Common/Extensions/ICollectionExtension.cs
@@ -13,5 +13,12 @@ namespace ITCFW.Common.Extensions
                 foreach (var item in list)
                     collection.Add(item);
         }
+
+        public static void RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> list)
+        {
+            if (list != null)
+                for (int index = list.Count() - 1; index >= 0; index--)
+                    collection.Remove(list.ElementAt(index));
+        }
     }
 }
---
+++ Yuhan.WPF/Extensions/ItemCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Yuhan.WPF.Extensions
+{
+    public static class ItemCollectionExtension
+    {
+        public static void RemoveRange<T>(this ItemCollection collection, IEnumerable<T> items)
+        {
+            if(items != null){
+                for (int index = items.Count() - 1; index >= 0; index--)
+                {
+                    collection.Remove(items.ElementAt(index));
+                }
+            }
+        }
+    }
+}
