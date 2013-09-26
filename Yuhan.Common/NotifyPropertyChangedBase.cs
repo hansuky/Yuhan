fadf41dff763a0caf905a80643dacf48bbfda770
Yuhan <hansuky@hotmail.com>
2013-09-26 20:19:27 +0900

    Add DataContract

--- Yuhan.Common/NotifyPropertyChangedBase.cs
+++ Yuhan.Common/NotifyPropertyChangedBase.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace Yuhan.Common.Models
 {
+    [DataContract]
     public class NotifyPropertyChangedBase : INotifyPropertyChanged
     {
         #region INotifyPropertyChanged Memebers
+        
         public event PropertyChangedEventHandler PropertyChanged;
         #endregion
 
