2de5826af37d53c8c6181565750ab5b78b039644
Yuhan <hansuky@hotmail.com>
2013-11-08 11:18:11 +0900

    Added Yuhna.Data.Excel

---
+++ Yuhan.Common/Events/ObjectEventArgs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yuhan.Common.Events
+{
+    public class ResultEventArgs : EventArgs
+    {
+        private Boolean isSuccess;
+
+        public Boolean IsSuccess
+        {
+            get { return isSuccess; }
+            set
+            {
+                isSuccess = value;
+                if (value)
+                    this.Exception = null;
+            }
+        }
+
+
+        public Object Object { get; set; }
+
+        private Exception exception;
+
+        public Exception Exception
+        {
+            get { return exception; }
+            set
+            {
+                exception = value;
+                if (value != null)
+                    this.IsSuccess = false;
+            }
+        }
+
+
+        public ResultEventArgs(Boolean isSuccess = true)
+            : base()
+        {
+            this.IsSuccess = isSuccess;
+        }
+        public ResultEventArgs(Exception exception)
+            : base()
+        {
+            this.Exception = exception;
+            this.IsSuccess = false;
+        }
+    }
+}
---
+++ Yuhan.Common/Events/TransactionEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yuhan.Common.Events
+{
+    public class TransactionEventArgs : EventArgs
+    {
+        public Boolean IsSuccess { get; set; }
+
+        public String Message { get; set; }
+
+        public TransactionEventArgs(Boolean isSuccess = true, String message = null)
+            : base()
+        {
+            this.IsSuccess = true;
+            this.Message = message;
+        }
+
+        public TransactionEventArgs(String message)
+            :base()
+        {
+            this.IsSuccess = true;
+            this.Message = message;
+        }
+    }
+
+    public delegate void TransactionEventHandler(Object sender, TransactionEventArgs e);
+}
