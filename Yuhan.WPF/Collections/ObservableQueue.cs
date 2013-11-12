6f24e3156b04e830ef8577f74c14d98c4a68e49b
Yuhan <hansuky@hotmail.com>
2013-11-12 18:53:35 +0900

    Added ObservableQueue

---
+++ Yuhan.WPF/Collections/ObservableQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Yuhan.Common.Collections
+{
+    public class ObservableQueue<T> : INotifyCollectionChanged, IEnumerable<T>
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        private readonly Queue<T> queue = new Queue<T>();
+
+        public void Enqueue(T item)
+        {
+            queue.Enqueue(item);
+            if (CollectionChanged != null)
+                CollectionChanged(this,
+                    new NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Add, item));
+        }
+
+        public T Dequeue()
+        {
+            var item = queue.Dequeue();
+            if (CollectionChanged != null)
+                CollectionChanged(this,
+                    new NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Remove, item));
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return queue.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
